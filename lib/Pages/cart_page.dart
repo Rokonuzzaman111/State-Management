2c2f97b2017024767d2f9c2568f746ab6004f0e0
Md. Rokonuzzaman <mdrokonuzzaman115@gmail.com>
2026-01-01 21:26:31 +0600

    Shoping Item counting details shows

---
+++ lib/Pages/cart_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_management/Provider/home_page_provider.dart';
+
+class CartPage extends StatefulWidget {
+  const CartPage({super.key});
+
+  @override
+  State<CartPage> createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 50,
+        backgroundColor: const Color.fromARGB(255, 153, 255, 255),
+        title: Text("Cart"),
+      ),
+      body: SafeArea(
+        child:ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+
+            SizedBox(height: 20,),
+           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Total Porduct Ammount", style: TextStyle(color: Colors.black, fontSize: 23, fontWeight: FontWeight.bold),),
+              SizedBox(height: 5,),
+
+                Consumer<ItemCount>(
+                  builder: (context, provider,child){ 
+                    
+             return Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(provider.count.toString()),
+                
+                  SizedBox(width: 200,),
+                  Text("Total",  style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.bold),)
+                  
+
+                ],
+               );
+              } 
+                 ), 
+              SizedBox(height: 25,),
+
+             
+                
+          
+
+            ],
+           )
+            
+
+
+          ],
+        )
+      
+      ),
+    );
+  }
+}
