c53fbc06828394cb16fb169214c2d9964a2db896
Md. Rokonuzzaman <mdrokonuzzaman115@gmail.com>
2026-01-01 18:54:35 +0600

    Wraping Material App to  MultiProvider for using many ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:state_management/Pages/home.dart';
+import 'package:state_management/Provider/home_page_provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,14 +13,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MultiProvider(
+      providers:[
+        ChangeNotifierProvider<HomePageProvider>(create: (_)=> HomePageProvider() )
+
+
+      ],
+    
+    
+    
+     child: MaterialApp(
       title: 'State Management',
       home: HomePage(),
   
       debugShowCheckedModeBanner: false,
 
     
-    );
+    )
+     );
   }
 }
 
